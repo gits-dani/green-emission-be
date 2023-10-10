b7c09beb237358e7cc2801963798522adcf97f6b
Muhammad Dani Haikal <45703647+muhammaddanihaikal@users.noreply.github.com>
2023-10-11 05:16:50 +0700

    feat/userProfile: add userProfile services

--- build/controllers/userProfileController.js
+++ build/controllers/userProfileController.js
@@ -8,44 +8,52 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
+const dayjs_1 = __importDefault(require("dayjs"));
 class UserProfileController {
     constructor() {
+        // tambah user profile
+        // -> jika belum ada user profile berdasarkan user_id, maka buat dulu data user profilenya
+        // -> jika sudah ada user profile berdasarkan user_id, maka update saja data user profilenya
         this.add = (req, res) => __awaiter(this, void 0, void 0, function* () {
             try {
                 // Ambil data dari req.body
-                const { nama, no_hp, tanggal_lahir, alamat, user_id } = req.body;
-                // Cek user di database
-                const user = yield this.prisma.user.findUnique({
-                    where: {
-                        id: parseInt(user_id),
-                    },
-                });
-                // Validasi: jika user tidak ditemukan
-                if (!user) {
-                    return res
-                        .status(404)
-                        .json({ status: "error", message: "User tidak ditemukan" });
-                }
+                const { nama, no_hp, tanggal_lahir, alamat } = req.body;
+                const user_id = parseInt(req.body.user_id);
+                const tanggal_lahir_iso = (0, dayjs_1.default)(tanggal_lahir).toISOString();
                 // Cari userProfile berdasarkan user_id
                 const userProfile = yield this.prisma.userProfile.findUnique({
                     where: {
-                        user_id: parseInt(user_id),
+                        id: user_id,
                     },
                 });
-                // Proses pembuatan userProfile
-                yield this.prisma.userProfile.create({
-                    data: {
-                        nama: nama || (userProfile === null || userProfile === void 0 ? void 0 : userProfile.nama) || "",
-                        no_hp: no_hp || (userProfile === null || userProfile === void 0 ? void 0 : userProfile.no_hp) || "",
-                        tanggal_lahir: tanggal_lahir || (userProfile === null || userProfile === void 0 ? void 0 : userProfile.tanggal_lahir) || "",
-                        alamat: alamat || (userProfile === null || userProfile === void 0 ? void 0 : userProfile.alamat) || "",
-                        user: {
-                            connect: {
-                                id: parseInt(user_id), // menghubungkan dengan tabel user
-                            },
+                // buat object user profile
+                const newUserProfile = {
+                    nama: nama || (userProfile === null || userProfile === void 0 ? void 0 : userProfile.nama) || "",
+                    no_hp: no_hp || (userProfile === null || userProfile === void 0 ? void 0 : userProfile.no_hp) || "",
+                    tanggal_lahir: tanggal_lahir_iso || (userProfile === null || userProfile === void 0 ? void 0 : userProfile.tanggal_lahir) || "",
+                    alamat: alamat || (userProfile === null || userProfile === void 0 ? void 0 : userProfile.alamat) || "",
+                    user: {
+                        connect: {
+                            id: user_id, // menghubungkan dengan tabel user
                         },
+                    },
+                };
+                // validasi: jika user profile belum ada maka tambah data
+                if (!userProfile) {
+                    yield this.prisma.userProfile.create({
+                        data: newUserProfile,
+                    });
+                }
+                // jika user profile sudah ada maka update data saja
+                yield this.prisma.userProfile.update({
+                    data: newUserProfile,
+                    where: {
+                        id: user_id,
                     },
                 });
                 // Response sukses
--- src/controllers/userProfileController.ts
+++ src/controllers/userProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import dayjs from "dayjs";
 
 class UserProfileController {
   private prisma: PrismaClient;
@@ -8,44 +9,48 @@ class UserProfileController {
     this.prisma = new PrismaClient();
   }
 
+  // tambah user profile
+  // -> jika belum ada user profile berdasarkan user_id, maka buat dulu data user profilenya
+  // -> jika sudah ada user profile berdasarkan user_id, maka update saja data user profilenya
   add = async (req: Request, res: Response) => {
     try {
       // Ambil data dari req.body
-      const { nama, no_hp, tanggal_lahir, alamat, user_id } = req.body;
-
-      // Cek user di database
-      const user = await this.prisma.user.findUnique({
-        where: {
-          id: parseInt(user_id),
-        },
-      });
-
-      // Validasi: jika user tidak ditemukan
-      if (!user) {
-        return res
-          .status(404)
-          .json({ status: "error", message: "User tidak ditemukan" });
-      }
+      const { nama, no_hp, tanggal_lahir, alamat } = req.body;
+      const user_id = parseInt(req.body.user_id);
+      const tanggal_lahir_iso = dayjs(tanggal_lahir).toISOString();
 
       // Cari userProfile berdasarkan user_id
       const userProfile = await this.prisma.userProfile.findUnique({
         where: {
-          user_id: parseInt(user_id),
+          id: user_id,
         },
       });
 
-      // Proses pembuatan userProfile
-      await this.prisma.userProfile.create({
-        data: {
-          nama: nama || userProfile?.nama || "", // Gunakan nilai default jika tidak ada data baru
-          no_hp: no_hp || userProfile?.no_hp || "",
-          tanggal_lahir: tanggal_lahir || userProfile?.tanggal_lahir || "",
-          alamat: alamat || userProfile?.alamat || "",
-          user: {
-            connect: {
-              id: parseInt(user_id), // menghubungkan dengan tabel user
-            },
+      // buat object user profile
+      const newUserProfile = {
+        nama: nama || userProfile?.nama || "", // Gunakan nilai default jika tidak ada data baru
+        no_hp: no_hp || userProfile?.no_hp || "",
+        tanggal_lahir: tanggal_lahir_iso || userProfile?.tanggal_lahir || "",
+        alamat: alamat || userProfile?.alamat || "",
+        user: {
+          connect: {
+            id: user_id, // menghubungkan dengan tabel user
           },
+        },
+      };
+
+      // validasi: jika user profile belum ada maka tambah data
+      if (!userProfile) {
+        await this.prisma.userProfile.create({
+          data: newUserProfile,
+        });
+      }
+
+      // jika user profile sudah ada maka update data saja
+      await this.prisma.userProfile.update({
+        data: newUserProfile,
+        where: {
+          id: user_id,
         },
       });
 
