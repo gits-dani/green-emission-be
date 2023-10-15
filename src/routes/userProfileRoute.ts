f12ec902e060c1a30d17d2ff9ed1dfd2369b270a
Muhammad Dani Haikal <45703647+muhammaddanihaikal@users.noreply.github.com>
2023-10-15 13:02:23 +0700

    feat(userProfile): add auth in user profile route and change test file

--- src/routes/userProfileRoute.ts
+++ src/routes/userProfileRoute.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import UserProfileController from "../controllers/userProfileController";
 import { upload } from "../middlewares/upload";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
 
 const route = Router();
 
-route.post("/user-profile", UserProfileController.add);
-route.get("/user-profile", UserProfileController.get);
+route.post("/user-profile", isAuthenticated, UserProfileController.add);
+route.get("/user-profile", isAuthenticated, UserProfileController.get);
 route.post(
   "/user-profile/upload-foto-profil",
+  isAuthenticated,
   upload.single("foto_profil"),
   UserProfileController.addFotoProfil
 );
